38be2c8764dc32aad8f45800df12d61e3266c0c9
prosper20 <bobsonconnect@gmail.com>
2025-05-22 16:52:39 +0100

    fix: styling issues

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 
-  @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 
   <style>
     :root {
